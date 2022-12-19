208dda6efafc28c1f963f13cdab676b9dc7bfd8b
Perov Yuri <iury.perov@gmail.com>
2022-12-19 16:52:14 +0500

    [FIX] Deleting broadcast in DNS

--- DNS/DnsNode.cs
+++ DNS/DnsNode.cs
@@ -2,7 +2,6 @@
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using Core.Network;
 using Core.Utils;
 using Microsoft.Extensions.Logging;
@@ -14,6 +13,8 @@ public class DnsNode : Node
     private readonly ILogger logger;
     private static readonly ConcurrentDictionary<IPEndPoint, long> Addresses = new();
 
+    private const int TtlSeconds = 3 * 60;
+
     public DnsNode(ILogger logger, IPAddress address, int port) : base(address, port)
     {
         this.logger = logger;
@@ -22,38 +23,17 @@ public class DnsNode : Node
     protected override void HandlePackage(Package package)
     {
         var addressFrom = package.AddressFrom;
+        
         logger.LogInformation($"Connect with {addressFrom}");
         
-        SendBroadcast(addressFrom);
-
+        var now = DateTimeOffset.Now.ToUnixTimeSeconds();
         var addresses = Addresses
-            .Keys
-            .Where(address => !address.Equals(addressFrom))
+            .Where(pair => !pair.Key.Equals(addressFrom) && now < pair.Value + TtlSeconds)
+            .Select(pair => pair.Key)
             .ToArray();
-        Addresses[addressFrom] = DateTime.Now.Ticks;
+        Addresses[addressFrom] = now;
 
         var responsePackage = new Package(AddressFrom, PackageTypes.Addresses, Serializer.ToBytes(addresses));
         Send(addressFrom, responsePackage);
-    }
-    
-    private void SendBroadcast(IPEndPoint addressFrom)
-    {
-        var package = new Package(AddressFrom, PackageTypes.Broadcast, Array.Empty<byte>());
-        
-        foreach (var address in Addresses
-                     .Keys
-                     .Where(point => !point.Equals(addressFrom))
-                     .ToArray())
-        {
-            try
-            {
-                Send(address, package);
-            }
-            catch (SocketException)
-            {
-                if (Addresses.TryRemove(address, out _))
-                    logger.LogInformation($"Disconnect node: {address}");
-            }
-        }
     }
 }
